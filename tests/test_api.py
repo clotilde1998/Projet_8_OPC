2a65c8699eef0a1b1d871bdeed98336120b9b26c
Clotilde Nadjinan <93580199+clotilde1998@users.noreply.github.com>
2025-06-26 15:57:06 +0200

    ajout test api

---
+++ tests/test_api.py
@@ -0,0 +1,30 @@
+# test_api.py
+from fastapi.testclient import TestClient
+from api import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "message" in response.json()
+
+def test_prediction_valid_client():
+    response = client.get("/prediction/128180")  # Remplace par un ID existant
+    assert response.status_code == 200
+    assert "probability_default" in response.json()
+
+def test_prediction_invalid_client():
+    response = client.get("/prediction/999999999")  # ID inexistant
+    assert response.status_code == 200
+    assert "error" in response.json()
+
+def test_shap_valid_client():
+    response = client.get("/interpretabilite/128180")  # Remplace par un ID existant
+    assert response.status_code == 200
+    assert "shap_values" in response.json()
+
+def test_shap_invalid_client():
+    response = client.get("/interpretabilite/999999999")
+    assert response.status_code == 200
+    assert "error" in response.json()
